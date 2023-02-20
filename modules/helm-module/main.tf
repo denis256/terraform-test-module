b9f3d804e1d5a8cefd9bce23801a5f111c2a4882
Denis O <denis.o@linux.com>
2023-02-20 20:06:31 +0000

    helm-module

---
+++ modules/helm-module/main.tf
@@ -0,0 +1,25 @@
+
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+      version = "2.9.0"
+    }
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    config_path = "~/.kube/config"
+  }
+}
+
+
+variable "enabled" {}
+
+resource "helm_release" "karpenter_config" {
+  count            = var.enabled ? 1 : 0
+  name             = "karpenter-config"
+  chart            = "./karpenter-config"
+}
+
